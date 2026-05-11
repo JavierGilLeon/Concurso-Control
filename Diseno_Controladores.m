c6ffb3243435f980f6658dad0250f8a5bacb7534
Joaquín Vera <cocopuebla2002@gmail.com>
2026-05-11 19:42:12 +0200

    Actualizacion 11/05/2026

--- Diseno_Controladores.m
+++ Diseno_Controladores.m
@@ -4,11 +4,11 @@
 
 clear; clc;
 
-% arámetros del Modelo 
+% Parámetros del Modelo 
 
-K   = 5.8;  % Ganancia estática
-tau = 42;   % Constante de tiempo (segundos)
-L   = 10;   % Retardo
+K   = 5.8;  
+tau = 42;   
+L   = 10;   
 
 disp('--- PARÁMETROS DE LA PLANTA ---');
 fprintf('K = %.2f | tau = %.2f s | L = %.2f s\n\n', K, tau, L);
@@ -22,7 +22,7 @@ disp('--- SINTONÍA AMIGO (PI) ---');
 fprintf('Kp = %.4f\n', Kp_amigo);
 fprintf('Ti = %.4f s\n\n', Ti_amigo);
 
-%% Diseño mediante método SIMC ****** MEJOR QUE EL OTRO ****
+%% Diseño mediante método SIMC 
 
 tau_c = L; 
 Kp_simc = (1/K) * (tau / (tau_c + L));
@@ -31,6 +31,20 @@ Ti_simc = min(tau, 4*(tau_c + L));
 disp('--- SINTONÍA SIMC (PI) ---');
 fprintf('Kp = %.4f\n', Kp_simc);
 fprintf('Ti = %.4f s\n', Ti_simc);
+
+
+%% 3. Diseño mediante método ITAE ***** MEJOR DE TODOS ****
+Kp_itae_pid = (1.357 / K) * (L / tau)^(-0.947);
+Ti_itae_pid = (tau / 0.842) * (L / tau)^(0.738);
+Td_itae_pid = tau * 0.381 * (L / tau)^(0.995);
+Kd_itae_pid = Kp_itae_pid * Td_itae_pid;
+
+disp('------------------------------------------------------');
+disp('--- 3. SINTONÍA ITAE (PID con c=0) ---');
+fprintf('Kp = %.4f\n', Kp_itae_pid);
+fprintf('Ti = %.4f s\n', Ti_itae_pid);
+fprintf('Kd = %.4f\n', Kd_itae_pid);
+
 
 %%
 % =========================================================================
@@ -45,7 +59,7 @@ K_eva =  1.0000;   % Te_evap
 K_tes = -0.2435;   %  Ts_TES
 K_tor =  0.3425;   %  Te_torref
 
-% Puntos de Operación (OP) de las perturbaciones
+% Puntos de Operación de las perturbaciones
 op_eva = 20.0;
 op_tes = 88.5;
 op_tor = 28.0;
